2f916974fbaf9ccbf8f7f64cf3282183464f9153
Hehaonan <stackoverflow163@163.com>
2020-05-04 16:01:47 +0800

    fix demo

--- app/src/main/java/com/android/hhn/kotlintoy/MainActivity.kt
+++ app/src/main/java/com/android/hhn/kotlintoy/MainActivity.kt
@@ -1,30 +1,17 @@
 package com.android.hhn.kotlintoy
 
-import android.animation.Animator
-import android.animation.ObjectAnimator
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.animation.doOnCancel
-import androidx.core.animation.doOnEnd
-import androidx.core.animation.doOnStart
 import androidx.core.content.edit
 import androidx.core.text.isDigitsOnly
-import androidx.core.util.lruCache
 import androidx.core.view.forEach
-import com.android.hhn.kotlintoy.coroutines.CommonUsage
-import com.android.hhn.kotlintoy.file.TestJava.main
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import org.w3c.dom.Text
-import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.coroutineContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -55,6 +42,7 @@ class MainActivity : AppCompatActivity() {
     private val mRequest = Request.Builder().url("https://www.baidu.com").get().build()
 
     private suspend fun getData(): String? {
+        // 异步任务
         val job = GlobalScope.async(AndroidCommonPool) {
             mOkHttpClient.newCall(mRequest).execute().body()?.string()
         }
@@ -65,63 +53,63 @@ class MainActivity : AppCompatActivity() {
         textView.text = "clear data"
     }
 
-//    private fun testCoroutine() {
-//        Log.d(TAG, "主线程id：${Thread.currentThread().id}")
-//        runBlocking {
-//            delay(1000)
-//            Log.d(TAG, "协程执行结束 -- 线程id：${Thread.currentThread().id}")
-//        }
-//        Log.d(TAG, "主线程执行结束")
-//
-//        Log.d(TAG, "主线程id：${Thread.currentThread().id}")
-//        val job = GlobalScope.launch(Dispatchers.Default, CoroutineStart.LAZY) {
-//            delay(2000)
-//            Log.d(TAG, "协程执行结束 -- 线程id：${Thread.currentThread().id}")
-//        }.start()
-//        Log.d(TAG, "主线程执行结束")
-//    }
-//
-//    fun testKTX() {
-//        getSharedPreferences("Space", Context.MODE_PRIVATE).edit(true) {
-//            putInt("key-int", 5)
-//            putString("key-str", "test")
-//        }
-//        main_ll.forEach {
-//            if (it is TextView) {
-//                it.text = "from KTX"
+    private fun testCoroutine() {
+        Log.d(TAG, "主线程id：${Thread.currentThread().id}")
+        runBlocking {
+            delay(1000)
+            Log.d(TAG, "协程执行结束 -- 线程id：${Thread.currentThread().id}")
+        }
+        Log.d(TAG, "主线程执行结束")
+
+        Log.d(TAG, "主线程id：${Thread.currentThread().id}")
+        val job = GlobalScope.launch(Dispatchers.Default, CoroutineStart.LAZY) {
+            delay(2000)
+            Log.d(TAG, "协程执行结束 -- 线程id：${Thread.currentThread().id}")
+        }.start()
+        Log.d(TAG, "主线程执行结束")
+    }
+
+    fun testKTX() {
+        getSharedPreferences("Space", Context.MODE_PRIVATE).edit(true) {
+            putInt("key-int", 5)
+            putString("key-str", "test")
+        }
+        main_ll.forEach {
+            if (it is TextView) {
+                it.text = "from KTX"
+            }
+        }
+        println("2342".isDigitsOnly())
+
+//        // Animator
+//        val animator = ObjectAnimator.ofFloat(main_content_tv, "alpha", 1.0f, 0.2f)
+//        // 常规
+//        animator.addListener(object : Animator.AnimatorListener {
+//            override fun onAnimationRepeat(animation: Animator?) {
 //            }
+//
+//            override fun onAnimationEnd(animation: Animator?) {
+//            }
+//
+//            override fun onAnimationCancel(animation: Animator?) {
+//            }
+//
+//            override fun onAnimationStart(animation: Animator?) {
+//            }
+//        })
+//        // KTX
+//        animator.doOnStart { }
+//        animator.doOnEnd { }
+//        animator.doOnCancel { }
+
+//        // 常规
+//        handler.postDelayed({
+//            // runnable.run()
+//        }, 1000L)
+//        // KTX
+//        handler.postDelayed(1000L) {
+//            // runnable.run()
 //        }
-//        println("2342".isDigitsOnly())
-//
-////        // Animator
-////        val animator = ObjectAnimator.ofFloat(main_content_tv, "alpha", 1.0f, 0.2f)
-////        // 常规
-////        animator.addListener(object : Animator.AnimatorListener {
-////            override fun onAnimationRepeat(animation: Animator?) {
-////            }
-////
-////            override fun onAnimationEnd(animation: Animator?) {
-////            }
-////
-////            override fun onAnimationCancel(animation: Animator?) {
-////            }
-////
-////            override fun onAnimationStart(animation: Animator?) {
-////            }
-////        })
-////        // KTX
-////        animator.doOnStart { }
-////        animator.doOnEnd { }
-////        animator.doOnCancel { }
-//
-////        // 常规
-////        handler.postDelayed({
-////            // runnable.run()
-////        }, 1000L)
-////        // KTX
-////        handler.postDelayed(1000L) {
-////            // runnable.run()
-////        }
-//    }
+    }
 
 }
--- app/src/main/java/com/android/hhn/kotlintoy/coroutines/TestCoroutine3.kt
+++ app/src/main/java/com/android/hhn/kotlintoy/coroutines/TestCoroutine3.kt
@@ -1,62 +1,29 @@
 package com.android.hhn.kotlintoy.coroutines
 
 import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
 /**
  * Author: haonan.he ;<p/>
  * Date: 2020/4/29,6:32 PM ;<p/>
- * Description: ;<p/>
+ * Description: 测试异步任务;<p/>
  * Other: ;
  */
-
-
 fun main() = runBlocking {
-    test1()
-}
-
-suspend fun test2() {
-    GlobalScope.launch {
-        val result1 = GlobalScope.async {
-            getResult1()
-        }
-        val result2 = GlobalScope.async {
-            getResult2()
-        }
-        val result = result1.await() + result2.await()
-        println("result = $result")
+    val time = measureTimeMillis {
+        val one = GlobalScope.async { getData1() }
+        val two = GlobalScope.async { getData2() }
+        println("The answer is ${one.await() + "-" + two.await()}")
     }
+    println("Completed in $time ms")
 }
 
-
-private suspend fun getResult1(): Int {
-    delay(3000)
-    return 1
+private suspend fun getData1(): String {
+    delay(1000)
+    return "data1"
 }
 
-private suspend fun getResult2(): Int {
-    delay(4000)
-    return 2
-}
-
-
-suspend fun test1() {
-    GlobalScope.launch {
-        val token = getToken()
-        val userInfo = getUserInfo(token)
-        setUserInfo(userInfo)
-    }.join()
-}
-
-private fun setUserInfo(userInfo: String) {
-    println(userInfo)
-}
-
-private suspend fun getToken(): String {
+private suspend fun getData2(): String {
     delay(2000)
-    return "token"
-}
-
-private suspend fun getUserInfo(token: String): String {
-    delay(3000)
-    return "$token - userInfo"
+    return "data2"
 }
